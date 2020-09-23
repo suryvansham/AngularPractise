9dc06cf701a39438b8f0c35c4ade5e95a6a031ae
Amit Raghuvanshi <amit@Amits-MacBook-Air.local>
2020-09-23 20:30:03 +0530

    keeping validation logic in component class

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -15,11 +15,11 @@ export class CreateEmployeeComponent implements OnInit {
   ngOnInit() {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
-      email : [''],
+      email : ['',Validators.required],
       skills: this.fb.group({
-        skillName : [''],
-        expInYrs : [''],
-        proficiency : ['']
+        skillName : ['',Validators.required],
+        expInYrs : ['',Validators.required],
+        proficiency : ['',Validators.required]
       })
     });
 
@@ -28,11 +28,12 @@ export class CreateEmployeeComponent implements OnInit {
     });
 
     this.employeeForm.valueChanges.subscribe(value=>{
-      console.log(JSON.stringify(value));
+      this.displayValidationMessages(this.employeeForm);
     })
     
   }
   onSubmit():void{
+    this.displayValidationMessages(this.employeeForm);
     this.logKeyValuePair(this.employeeForm);
     console.log(this.employeeForm.value);
   }
@@ -61,6 +62,56 @@ export class CreateEmployeeComponent implements OnInit {
          console.log('Key ' + key + ',' + 'Value ' + abstractControl.value);
        }
     });
+  }
+
+  //This object contains all the validation messages displayed to the user
+  formErrors = {
+    'fullName' : '',
+    'email' : '',
+    'skillName' : '',
+    'expInYrs' : '',
+    'proficiency' : ''
+  }
+
+  //It contains all validation msz for the form.
+  validationMessage = {
+    'fullName' : {
+      'required' : 'fullName is required',
+      'minlength': 'Full Name must be greater than 2 characters.',
+    'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  }
+
+  //logic to display validation msz
+  displayValidationMessages(group : FormGroup) : void{
+    Object.keys(group.controls).forEach((key : string)=>{
+      const abstractControl = group.get(key);
+      if(abstractControl instanceof FormGroup){
+        this.displayValidationMessages(abstractControl);
+      }
+      else{
+        this.formErrors[key] = '';
+        if(abstractControl && !abstractControl.valid){
+          const message =  this.validationMessage[key];
+            for(const errorKey in abstractControl.errors){
+              this.formErrors[key] += message[errorKey];
+            }
+          };
+        }
+      
+    })
   }
 
 }
